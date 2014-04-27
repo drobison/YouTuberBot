5c226ca70162002f7b6c36b8c8aa207966e41987
David Robison <davidrobison122@hotmail.com>
2014-04-27 14:17:56 -0700

    Changed indentation to tabs

--- util.py
+++ util.py
@@ -2,24 +2,24 @@
 import re, time
 
 def formatted(*args):
-    now = time.strftime("%Y-%m-%d %H:%M:%S")
-    return "["+now+"] "+" ".join(map(str,args))
+	now = time.strftime("%Y-%m-%d %H:%M:%S")
+	return "["+now+"] "+" ".join(map(str,args))
 
 
 def log(*args):
-    print apply(formatted,args)
+	print apply(formatted,args)
 
 def fail(*args):
-    print '\033[91m'+apply(formatted,args)+'\033[0m'
+	print '\033[91m'+apply(formatted,args)+'\033[0m'
 
 def warn(*args):
-    print '\033[93m'+apply(formatted,args)+'\033[0m'
+	print '\033[93m'+apply(formatted,args)+'\033[0m'
 
 def success(*args):
-    print '\033[92m'+apply(formatted,args)+'\033[0m'
-    
+	print '\033[92m'+apply(formatted,args)+'\033[0m'
+
 def special(*args):
-    print '\033[95m'+apply(formatted,args)+'\033[0m'
-    
+	print '\033[95m'+apply(formatted,args)+'\033[0m'
+
 def bluelog(*args):
-    print '\033[94m'+apply(formatted,args)+'\033[0m'
+	print '\033[94m'+apply(formatted,args)+'\033[0m'
